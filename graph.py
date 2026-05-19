43f998349d9bfb0a8c6f599ea2f4139051aa8eba
richardm1234 <richard.mayer@tum.de>
2026-05-19 16:13:20 +0200

    line graph

--- graph.py
+++ graph.py
@@ -1,5 +1,13 @@
 import networkx as nx
 
+
 def createGraph():
+    """Generates a 3-regular graph"""
     G = nx.random_regular_graph(3, 10)
-    nx.draw(G)
+    nx.draw(G, with_labels = True)
+    return G
+
+def convertToLineGraph(G):
+    LG = nx.line_graph(G)
+    nx.draw(LG, with_labels = True)
+    return LG
