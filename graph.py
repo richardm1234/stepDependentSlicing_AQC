a50706e3c7ccbbc0253c88ab8fc80fe70054441a
richardm1234 <richard.mayer@tum.de>
2026-05-21 17:36:13 +0200

    example circuit and 4 node graph for testing purposes

--- graph.py
+++ graph.py
@@ -1,4 +1,25 @@
 import networkx as nx
+from qiskit import QuantumCircuit
+
+def createExampleCircuit(G):
+    """Converts the 4 node graph to a QAOA circuit from the paper for testing purposes"""
+    gamma = 0.5
+    beta = 0.3
+    circuit = QuantumCircuit(4)
+    for i in range(4):
+        circuit.h(i)
+
+    for u,v in G.edges():
+        circuit.rzz(2*gamma, u, v)
+
+    for i in range(4):
+        circuit.rx(2*beta, i)
+    return circuit
+
+def createNodeGraph():
+    """Creates a basic 4 node graph for testing purposes"""
+    G = nx.random_regular_graph(3, 4)
+    return G
 
 
 def createGraph():
@@ -11,3 +32,4 @@ def convertToLineGraph(G):
     LG = nx.line_graph(G)
     nx.draw(LG, with_labels = True)
     return LG
+
