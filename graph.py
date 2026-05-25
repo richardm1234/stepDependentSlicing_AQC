7e4d88b39396393158e1d61610e25537f2abadfd
richardm1234 <richard.mayer@tum.de>
2026-05-25 18:51:26 +0200

    findOptimalS now also returns which node to slice

--- graph.py
+++ graph.py
@@ -182,33 +182,33 @@ def findOptimalS(G, widths, order):
         order (list[int]): the initial ordering
 
     Returns:
-        list[int], int, list[int], int: new ordering with less contraction width, the width profile
-        and the optimal step S at which to slice r highest vertices
+        list[int], int, list[int], int, int: new ordering with less contraction width, the width profile
+        optimal step S and which vertices to slice
     """
     peak = widths.index(max(widths))
     minCWidth = sys.maxsize
     optimalS = 0
+    toSlice = None
     newWidths = []
     newOrdering = []
 
     for s in range(peak):
         G_copy = G.copy()
-
         for v in order[:s]:
             contract(G_copy, v)
-
         maxNeighbors = sorted(G_copy.nodes(), key=lambda v: G_copy.degree(v), reverse=True)[0]
         G_copy.remove_node(maxNeighbors)
         newOrder, width, neighbors = greedy(G_copy)
-
         if width < minCWidth:
             newOrdering = newOrder
             minCWidth = width
             newWidths = neighbors
             optimalS = s
+            toSlice = maxNeighbors
 
 
-    return newOrdering, minCWidth, newWidths, optimalS
+
+    return newOrdering, minCWidth, newWidths, optimalS, toSlice
 
 def stepDependentSlicing(LG, order, n, r):
     """
