7e7816424d4623f3de1ea24bcc4012ffe6c88dae
richardm1234 <richard.mayer@tum.de>
2026-06-11 13:47:31 +0200

    iterations of n // r times and step dependent slicing starts each iteration at startS = 0

--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
 from ordering import *
 
-def findOptimalS(G, widthProfile, order, startS=0):
+def findOptimalS(G, widthProfile, order, startS=0, r=1):
     """
     Find the optimal step s
 
@@ -8,10 +8,12 @@ def findOptimalS(G, widthProfile, order, startS=0):
         G (nx.Graph): line graph
         widthProfile (list[int]): the degrees of each node
         order (list[int]): the initial ordering
+        startS: the search for optimal S begins here
+        r: number of vertices to slices at step S
 
     Returns:
-        list[int], list[int], int, int, int: new ordering, new width profile, new contraction width,
-        optimal step S and which vertices to slice
+        list[int], list[int], int, int, list[int]: new ordering, new width profile, new contraction width,
+        optimal step S and the r vertices to slice at step S
     """
     peak = widthProfile.index(max(widthProfile))
     minWidth = sys.maxsize
@@ -24,15 +26,18 @@ def findOptimalS(G, widthProfile, order, startS=0):
         G_copy = G.copy()
         for v in order[:s]:
             contract(G_copy, v)
-        maxNeighbors = sorted(G_copy.nodes(), key=lambda v: G_copy.degree(v), reverse=True)[0]
-        G_copy.remove_node(maxNeighbors)
+        sortedDeg = sorted(G_copy.nodes(), key=lambda v: G_copy.degree(v), reverse=True)
+        minR = min(len(sortedDeg), r)
+        rMaxNeighbors = sortedDeg[:minR]
+        for v in rMaxNeighbors:
+            G_copy.remove_node(v)
         newOrder, neighbors, width = greedy(G_copy)
         if width < minWidth:
             newOrdering = newOrder
             minWidth = width
             newWidthProfile = neighbors
             optimalS = s
-            toSlice = maxNeighbors
+            toSlice = rMaxNeighbors
 
     return newOrdering, newWidthProfile, minWidth, optimalS, toSlice
 
@@ -48,18 +53,19 @@ def stepDependentSlicing(LG, order, widthProfile, n=2, r=1):
         r (int): number of nodes to slice per step
 
     Returns:
-        dict[int, int]: optimal S and their corresponding slice nodes
+        dict[int, list[int]]: optimal S and their corresponding slice nodes
     """
     schedule = {}
     startS = 0
-    for _ in range(n):
-        newOrder, newWidthProfile, minWidth, optimalS, toSlice = findOptimalS(LG, widthProfile, order, startS)
+    for _ in range(n//r):
+        newOrder, newWidthProfile, minWidth, optimalS, toSlice = findOptimalS(LG, widthProfile, order, startS, r)
         if toSlice is None:
             break
         schedule[optimalS] = toSlice
         order = newOrder
         widthProfile = newWidthProfile
-        startS = optimalS + 1
+        startS = 0
+
 
     return schedule
 
